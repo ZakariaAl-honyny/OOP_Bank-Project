9871b841c8b20eca417a57d0f44090ef2517a612
Zakaria Al-onyny <zkryaalhnyny0@gmail.com>
2024-10-26 05:17:58 -0700

    Modify Main.cpp file, and add clsDate.h and clsInputValidate.h libraries

--- Main.cpp
+++ Main.cpp
@@ -1,20 +1,77 @@
 
-
 #include <iostream>
 #include "clsBankClient.h";
+#include "clsInputValidate.h";
 
-using namespace std;
+void ReadClientInfo(clsBankClient& Client)
+{
+    
+    cout << "\nEnter FirstName: ";
+    Client.FirstName = clsInputValidate::ReadString();
+
+    cout << "\nEnter LastName: ";
+    Client.LastName = clsInputValidate::ReadString();
+
+    cout << "\nEnter Email: ";
+    Client.Email = clsInputValidate::ReadString();
+
+    cout << "\nEnter Phone: ";
+    Client.Phone = clsInputValidate::ReadString();
+
+    cout << "\nEnter PinCode: ";
+    Client.PinCode = clsInputValidate::ReadString();
+
+    cout << "\nEnter Account Balance: ";
+    Client.AccountBalance = clsInputValidate::ReadfloatNumber();
+}
+
+void UpdateClient()
+{
+    string AccountNumber = "";
+
+    cout << "\nPlease Enter Client Account Number: ";
+    AccountNumber = clsInputValidate::ReadString();
+
+    while (!clsBankClient::IsClientExist(AccountNumber))
+    {
+        cout << "\nAccount number is not found, chooce anther one: ";
+        AccountNumber = clsInputValidate::ReadString();
+    }
+
+    clsBankClient Client1 = clsBankClient::Find(AccountNumber);
+    Client1.Print();
+
+
+    cout << "\nUpdate Client Info:";
+    cout << "\n______________________\n";
+
+    ReadClientInfo(Client1);
+
+    clsBankClient::enSaveResults SaveResult;
+
+    SaveResult = Client1.Save();
+
+    switch (SaveResult)
+    {
+    case clsBankClient::enSaveResults::svSucceeded:
+    {
+        cout << "\nAccount Update successfully :-).\n";
+        Client1.Print();
+        break;
+    }
+    case clsBankClient::enSaveResults::svFaildEmptyObject:
+    {
+        cout << "\nError Account was not saved decause it's Empty.";
+        break;
+    }
+
+    }
+}
 
 int main()
 {
-    
-    clsBankClient Client1 = clsBankClient::Find("A101");
-    Client1.Print();
-
-    clsBankClient Client2 = clsBankClient::Find("A101", "1234");
-    Client2.Print();
-
-    cout << Client2.IsEmpty() << endl;
-
-    cout << clsBankClient::IsClientExist("A101") << endl;
+    UpdateClient();
+    system("pause>0");
+    return 0;
 }
+
