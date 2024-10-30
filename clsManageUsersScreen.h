96379b1fa64e89a4845cf929799665e5c775b2e9
Zakaria Al-onyny <zkryaalhnyny0@gmail.com>
2024-10-30 05:53:56 -0700

    Modified  clsMainScreen.h, and added clsManageUsersScreen.h

---
+++ clsManageUsersScreen.h
@@ -0,0 +1,138 @@
+#pragma once
+#include <iostream>
+#include "clsScreen.h"
+#include "clsInputValidate.h"
+#include <iomanip>
+
+using namespace std;
+
+class clsManageUsersScreen : protected clsScreen
+{
+private:
+
+	enum enMangeUsersOptions {
+		eListUsers = 1, eAddNewUser = 2, eDeleteUser = 3,
+		eUpdateUser = 4, eFindUser = 5, eMainMenue = 6
+	};
+
+	static short _ReadMangeUsersMenueOption()
+	{
+		cout << setw(37) << left << "" << "Choose what do you want to do? [1 to 6]? ";
+		short Choice = 0;
+		Choice = clsInputValidate::ReadShortNumberBetween(1, 6, "\t\t\t\t\tEnter Number Between 1 to 6 ? ");
+
+		return Choice;
+	}
+
+	static void _GoBackToMangeUsersMenue()
+	{
+		cout << "\n\nPress any key to go back to Mange Users menue...";
+		system("pause>0");
+		ShowMangeUsersMenue();
+	}
+
+	static void _ShowListUsersScreen()
+	{
+		cout << "\nList Users Screen will be here soon...\n";
+	}
+
+	static void _ShowAddNewUserScreen()
+	{
+		cout << "\nAdd New User Screen will be here soon...\n";
+	}
+
+	static void _ShowDeleteUserScreen()
+	{
+		cout << "\nDelete User Screen will be here soon...\n";
+	}
+
+	static void _ShowUpdateUserScreen()
+	{
+		cout << "\nUpdate User Screen will be here soon...\n";
+	}
+
+	static void _ShowFindUserScreen()
+	{
+		cout << "\nFind User Screen will be here soon...\n";
+	}
+
+	static void _PerformMangeUsersMenueOption(enMangeUsersOptions MangeUsersMenueOption)
+	{
+		switch (MangeUsersMenueOption)
+		{
+		case enMangeUsersOptions::eListUsers:
+		{
+			system("cls");
+			_ShowListUsersScreen();
+			_GoBackToMangeUsersMenue();
+			break;
+		}
+
+		case enMangeUsersOptions::eAddNewUser:
+		{
+			system("cls");
+			_ShowAddNewUserScreen();
+			_GoBackToMangeUsersMenue();
+			break;
+		}
+
+		case enMangeUsersOptions::eDeleteUser:
+		{
+			system("cls");
+			_ShowDeleteUserScreen();
+			_GoBackToMangeUsersMenue();
+			break;
+		}
+
+		case enMangeUsersOptions::eUpdateUser:
+		{
+			system("cls");
+			_ShowUpdateUserScreen();
+			_GoBackToMangeUsersMenue();
+			break;
+		}
+
+		case enMangeUsersOptions::eFindUser:
+		{
+			system("cls");
+			_ShowFindUserScreen();
+			_GoBackToMangeUsersMenue();
+			break;
+		}
+
+		case enMangeUsersOptions::eMainMenue:
+		{
+			//do nothing here the main screen will handle it :-); 
+			//ShowMainMenue();
+		}
+
+		}
+	}
+
+	
+
+	
+
+public:
+
+	static void ShowMangeUsersMenue()
+	{
+
+		system("cls");
+		_DrawScreenHeader("\tManage Users Screen");
+
+		cout << setw(37) << left << "" << "=============================================\n";
+		cout << setw(37) << left << "" << "\t       Mange Users  Menue\n";
+		cout << setw(37) << left << "" << "=============================================\n";
+		cout << setw(37) << left << "" << "\t[1] List Users.\n";
+		cout << setw(37) << left << "" << "\t[2] Add New User.\n";
+		cout << setw(37) << left << "" << "\t[3] Delete User.\n";
+		cout << setw(37) << left << "" << "\t[4] Update User.\n";
+		cout << setw(37) << left << "" << "\t[5] Find User.\n";
+		cout << setw(37) << left << "" << "\t[6] Main Menue.\n";
+		cout << setw(37) << left << "" << "=============================================\n";
+		_PerformMangeUsersMenueOption((enMangeUsersOptions)_ReadMangeUsersMenueOption());
+	}
+
+};
+
